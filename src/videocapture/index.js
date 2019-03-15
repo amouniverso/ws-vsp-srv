5328c8296370cb3b19324830b18f05a89a1b2cd7
Filipp <amouniverso@yandex.ru>
2019-03-15 16:50:46 +0300

    optimization

--- src/videocapture/index.js
+++ src/videocapture/index.js
@@ -1,32 +1,52 @@
 const cv = require('opencv4nodejs');
+const path = require('path');
 function startVideoCapture(ws) {
     const devicePort = 0;
     const vCap = new cv.VideoCapture(devicePort);
 
     // loop through the capture
-    const delay = 130;
+    const delay = 30;
     let done = false;
+    let count = 0;
+
+    const faceClassifier = new cv.CascadeClassifier(cv.LBP_FRONTALFACE_IMPROVED); //LBP_FRONTALFACE_IMPROVED HAAR_FRONTALFACE_ALT2
+    const eyesClassifier = new cv.CascadeClassifier(cv.HAAR_EYE);
+
+    let faceObjects = null;
+    let eyesObjects = null;
     while (!done) {
-        //console.log('read');
         let frame = vCap.read();
         if (frame.empty) {
             vCap.reset();
             frame = vCap.read();
         }
 
-        const faceClassifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
-        const eyesClassifier = new cv.CascadeClassifier(cv.HAAR_EYE);
+        if (count === 10) {
+            count = 0;
+            const grayFrame = frame.bgrToGray();
+            faceObjects = faceClassifier.detectMultiScale(grayFrame).objects;
+            eyesObjects = eyesClassifier.detectMultiScale(grayFrame).objects;
 
-        const grayFrame = frame.bgrToGray();
-        const faceObjects = faceClassifier.detectMultiScale(grayFrame).objects;
-        const eyesObjects = eyesClassifier.detectMultiScale(grayFrame).objects;
+        }
+
         if (faceObjects && faceObjects.length > 0) {
             frame.drawRectangle(faceObjects[0], new cv.Vec3(255, 255, 255), 4);
+            if (eyesObjects && eyesObjects.length > 1) {
+                frame.drawRectangle(eyesObjects[0], new cv.Vec3(0, 255, 0), 2);
+                frame.drawRectangle(eyesObjects[1], new cv.Vec3(0, 255, 0), 2);
+            }
         }
-        if (eyesObjects && eyesObjects.length > 1) {
-            frame.drawRectangle(eyesObjects[0], new cv.Vec3(0, 255, 0), 2);
-            frame.drawRectangle(eyesObjects[1], new cv.Vec3(0, 255, 0), 2);
-        }
+
+        // const modelPath = path.resolve(__dirname, '../../../data/dnn/facenet');
+        // const prototxt = path.resolve(modelPath, 'facenet.prototxt');
+        // const modelFile = path.resolve(modelPath, 'res10_300x300_ssd_iter_140000.caffemodel');
+        //
+        // const net = new cv.readNetFromCaffe(prototxt, modelFile);
+        // //(h, w) = image.shape[:2]
+        // const blob = cv.blobFromImage(frame);
+        // net.setInput(blob);
+        // const detections = net.forward();
+        // console.log(detections);
 
         //cv.imshow('test frame', frame);
         // convert image to rgba color space
@@ -34,6 +54,7 @@ function startVideoCapture(ws) {
             ? frame.cvtColor(cv.COLOR_GRAY2RGBA)
             : frame.cvtColor(cv.COLOR_BGR2RGBA);
         ws.send(frameRGBA.getData());
+        console.log(count++);
         cv.waitKey(delay);
     }
 }
