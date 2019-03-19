fea23a409219b6139198b76c561f3acbefbd22fe
Filipp <amouniverso@yandex.ru>
2019-03-19 15:18:53 +0300

    dnn facenet detection

--- src/videocapture/index.js
+++ src/videocapture/index.js
@@ -1,19 +1,25 @@
 const cv = require('opencv4nodejs');
 const path = require('path');
+
+const faceClassifier = new cv.CascadeClassifier(cv.LBP_FRONTALFACE_IMPROVED); //LBP_FRONTALFACE_IMPROVED HAAR_FRONTALFACE_ALT2
+const eyesClassifier = new cv.CascadeClassifier(cv.HAAR_EYE);
+let faceObjects = null;
+let eyesObjects = null;
+let count = 0;
+
+const modelPath = path.resolve(__dirname, '../../../dnn/facenet');
+const prototxt = path.resolve(modelPath, 'deploy.prototxt');
+const modelFile = path.resolve(modelPath, 'res10_300x300_ssd_iter_140000.caffemodel');
+const net = new cv.readNetFromCaffe(prototxt, modelFile);
+
 function startVideoCapture(ws) {
     const devicePort = 0;
     const vCap = new cv.VideoCapture(devicePort);
 
     // loop through the capture
-    const delay = 30;
+    const delay = 1;
     let done = false;
-    let count = 0;
 
-    const faceClassifier = new cv.CascadeClassifier(cv.LBP_FRONTALFACE_IMPROVED); //LBP_FRONTALFACE_IMPROVED HAAR_FRONTALFACE_ALT2
-    const eyesClassifier = new cv.CascadeClassifier(cv.HAAR_EYE);
-
-    let faceObjects = null;
-    let eyesObjects = null;
     while (!done) {
         let frame = vCap.read();
         if (frame.empty) {
@@ -21,32 +27,8 @@ function startVideoCapture(ws) {
             frame = vCap.read();
         }
 
-        if (count === 10) {
-            count = 0;
-            const grayFrame = frame.bgrToGray();
-            faceObjects = faceClassifier.detectMultiScale(grayFrame).objects;
-            eyesObjects = eyesClassifier.detectMultiScale(grayFrame).objects;
-
-        }
-
-        if (faceObjects && faceObjects.length > 0) {
-            frame.drawRectangle(faceObjects[0], new cv.Vec3(255, 255, 255), 4);
-            if (eyesObjects && eyesObjects.length > 1) {
-                frame.drawRectangle(eyesObjects[0], new cv.Vec3(0, 255, 0), 2);
-                frame.drawRectangle(eyesObjects[1], new cv.Vec3(0, 255, 0), 2);
-            }
-        }
-
-        // const modelPath = path.resolve(__dirname, '../../../data/dnn/facenet');
-        // const prototxt = path.resolve(modelPath, 'facenet.prototxt');
-        // const modelFile = path.resolve(modelPath, 'res10_300x300_ssd_iter_140000.caffemodel');
-        //
-        // const net = new cv.readNetFromCaffe(prototxt, modelFile);
-        // //(h, w) = image.shape[:2]
-        // const blob = cv.blobFromImage(frame);
-        // net.setInput(blob);
-        // const detections = net.forward();
-        // console.log(detections);
+        openCVFaceDetection(frame);
+        //facenetFaceDetection(frame);
 
         //cv.imshow('test frame', frame);
         // convert image to rgba color space
@@ -54,9 +36,57 @@ function startVideoCapture(ws) {
             ? frame.cvtColor(cv.COLOR_GRAY2RGBA)
             : frame.cvtColor(cv.COLOR_BGR2RGBA);
         ws.send(frameRGBA.getData());
-        console.log(count++);
         cv.waitKey(delay);
     }
+}
+
+function openCVFaceDetection(frame) {
+    if (count === 10) {
+        count = 0;
+        const grayFrame = frame.bgrToGray();
+        faceObjects = faceClassifier.detectMultiScale(grayFrame).objects;
+        eyesObjects = eyesClassifier.detectMultiScale(grayFrame).objects;
+    }
+
+    if (faceObjects && faceObjects.length > 0) {
+        frame.drawRectangle(faceObjects[0], new cv.Vec3(255, 255, 255), 4);
+        if (eyesObjects && eyesObjects.length > 1) {
+            frame.drawRectangle(eyesObjects[0], new cv.Vec3(0, 255, 0), 2);
+            frame.drawRectangle(eyesObjects[1], new cv.Vec3(0, 255, 0), 2);
+        }
+    }
+    count++;
+}
+
+function facenetFaceDetection(frame) {
+    const blob = cv.blobFromImage(frame.resize(300, 300), 1.0, new cv.Size(300, 300),
+        new cv.Vec3(104.0, 177.0, 123.0), false, false);
+    net.setInput(blob);
+    let outputBlob = net.forward();
+    outputBlob = outputBlob.flattenFloat(outputBlob.sizes[2], outputBlob.sizes[3]);
+
+    const results = Array(outputBlob.rows).fill(0)
+        .map((res, i) => {
+            const className = outputBlob.at(i, 1);
+            const confidence = outputBlob.at(i, 2);
+            const topLeft = new cv.Point2(
+                outputBlob.at(i, 3) * frame.cols,
+                outputBlob.at(i, 6) * frame.rows
+            );
+            const bottomRight = new cv.Point2(
+                outputBlob.at(i, 5) * frame.cols,
+                outputBlob.at(i, 4) * frame.rows
+            );
+
+            return ({
+                className,
+                confidence,
+                topLeft,
+                bottomRight
+            })
+        });
+
+    frame.drawRectangle(results[0].topLeft, results[0].bottomRight, new cv.Vec3(0, 255, 0), 2);
 }
 
 module.exports = {
